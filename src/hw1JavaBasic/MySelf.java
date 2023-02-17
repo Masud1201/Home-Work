c6c70b31255a1d1e401bf1435463040930478fae
Masud1201 <masudrana1201@gmail.com>
2023-02-17 16:49:15 -0500

    update

--- src/hw1JavaBasic/MySelf.java
+++ src/hw1JavaBasic/MySelf.java
@@ -15,9 +15,8 @@ public class MySelf {
 	// actually method name is main represented by
 	// public static void main(String[] args) {
 	// }
-
 	public static void main(String[] args) { 
-		System.out.println(" Masud Rana");
+		System.out.println(" \'Masud Rana\"");
 		System.out.println("9730 77th street. Ozonepark,NY");
 
 	}
